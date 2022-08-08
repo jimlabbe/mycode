c48a3af70e840780c2ae6899f7402bde8767070c
Jim Labbe <jimlabbe@users.noreply.github.com>
2022-08-08 18:14:12 +0000

    pulling data from API

---
+++ week2/tupletraining.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#tuples are immutable
+#s is for string, d is for integers, f is for float
+demo_tuple=("Welcome", 2, "Python")
+
+c_string="%s to week %d of %s Training!"
+print(c_string % demo_tuple)
+
+
