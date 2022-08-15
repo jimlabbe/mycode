eda2e8af776f832c39ffc3ff11af8646230e677c
Jim Labbe <jimlabbe@users.noreply.github.com>
2022-08-15 21:07:38 +0000

    Web scraper success

--- parmesean.py
+++ parmesean.py
@@ -1,33 +1,71 @@
 #!/usr/bin/Python3
 '''ParmeSean is a webscraper that finds weekly meal prep recipes and emails them to you'''
-import requests
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
 from bs4 import BeautifulSoup as bs
 
 
-def scrape_web():
-    url = "https://www.tasteofhome.com/recipes/1/?_cooking-style=easy&_meal-types=lunch"
+def scrape_web(url):
     agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) Chrome/59.0.3071.115'}
     #Some websites don't like being scraped, but including an Agent browser fools the blocker.
     #Thanks StackOverflow
+    
+    try:
+        with closing(get(url, stream=True, headers=agent)) as results: 
+        #stream=True means requests cant release the connection until closed
+        #closing() closes results at the end of the block
 
-    page = requests.get(url, headers=agent) #requests the page data
-    soup = bs(page.text, "html.parser")
-    results = soup.find_all("span", id="html-attribute-name") #how find element?
-    print(results)
+            if html_checker(results):
+                return results.content
+            # .content() reads HTML off the requests object  
+            else:
+                return None
+    
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+def html_checker(results):
+    #check if response it html, return true if so
+    results_type = results.headers['Content-Type'].lower()
+    return (results.status_code == 200
+        and results_type is not None
+        and results_type.find('html') > -1)
+
+def log_error(e):
+    print(e)
+
+#    soup = bs(page.text, "html.parser")
+#    results = soup.find_all("span", id="html-attribute-name") #how find element?
+#    print(results)
     #content is chosen over .text to avoid problems with character encoding. The second argument ensures
     #that you use the appropriate parser for HTML content
 
 
 
 
-
 def main():
-    scrape_web()
-# scrape the website
+    url = scrape_web("https://www.tasteofhome.com/recipes/?_cooking-style=easy&_meal-types=lunch")
+    if url is not None:
+        html = bs(url, "html.parser")
+    recipes = set()
+    #sets are used over lists as it handles unorganized data better. It only permits unique values
+    
+    #data= html.select("script[type=application/ld+json]")
+    data= html.find_all("script",type="application/ld+json")
+    print(data)
+    
+    #for rname in html.select('ItemListElement.json()'):
+        #print(rname)
+        #for each in rname.text.split('\n'):
+        #print(
+        #return list(recipes)
+    #raise Exception('Error retrieving contents at {}'.format(url))
 
-# email the results
 
-# monitor an inbox
+
+    #print(url)
 
 
 if __name__ == "__main__":
